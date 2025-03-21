31db7c55e359a2ae23624c371b0917f15c62fc19
Sanjana5523 <ganjisanjana55@gmail.com>
2025-03-21 11:33:36 +0530

    first commit

---
+++ src/components/App8.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./App8.css"
+export default function App8() {
+  return (
+    <div className="main" >
+      <div >
+        <div className="col"><h2>React store</h2>
+       <div> <a href="#"> Home |</a>
+        <a href="#"> Cart |</a>
+        <a href="#"> Login </a>
+        </div>
+        </div>
+      <div>
+        <h3>Registration Form</h3>
+        <p><input type="name" placeholder="Enter name" /></p>
+        <p><input type="email" placeholder="Email address" /></p>
+        <p><input type="password" placeholder="New Password" /></p>
+        <p ><button>Submit</button></p>
+        <div> <a href="#" >Already have an Account? Log In</a></div>
+        <p>@Copyright 2025.All rights reserved.</p>
+      </div>
+    </div>
+    </div>
+  );
+}
